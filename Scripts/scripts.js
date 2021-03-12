c89775db383d15c3df51f2e92f7944073eff8679
David Preininger <dp2921@student.uni-lj.si>
2021-03-12 15:33:46 +0100

    Prijava uporabnika dodelana tudi v primeru, ko POST request faila

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,26 +3,34 @@ $("#inputButton").click(function () {
     let name = $("#nameInput").val();
     let lastName = $("#surnameInput").val();
 
-    let obj = {
-        "Name": name,
-        "LastName": lastName
-    }
+    if (name.length == 0 || lastName.length == 0) {
+        alert("Vpisite ime in priimek!");
+    } else {
 
-    console.log(obj);
-
-    let url = "https://localhost:44344/api/user/generate";
-
-    let id;
-
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: obj,
-        success: function (data, textStatus, jqXHR) {
-            var d = new Date();
-            d.setTime(d.getTime() + (3650 * 24 * 60 * 60 * 1000));
-            document.cookie = "UserId=" + data + "; expires=" + d + "; path=/";
-            window.location.href = '/'
+        let obj = {
+            "Name": name,
+            "LastName": lastName
         }
-    });
+
+
+        let url = "https://localhost:44344/api/user/generate";
+
+        let id;
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: obj,
+            success: function (data) {
+                var d = new Date();
+                d.setTime(d.getTime() + (3650 * 24 * 60 * 60 * 1000));
+                document.cookie = "UserId=" + data + "; expires=" + d + "; path=/";
+                window.location.href = '/'
+                
+            },
+            error: function () {
+                alert("Prislo je do napake, poskusite ponovno!");
+            }
+        });
+    }
 })
