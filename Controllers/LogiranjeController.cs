38692e8ac8c9a062382a7e337e7e182795415c61
David Preininger <dp2921@student.uni-lj.si>
2021-03-14 11:23:07 +0100

    Sprememba imen, izboljsanje delovanja podajanja idLokacije

--- Controllers/LogiranjeController.cs
+++ Controllers/LogiranjeController.cs
@@ -12,6 +12,8 @@ namespace LogiranjeApp.Controllers
     {
         public ActionResult Dodaj(int id)
         {
+            //Session["locationId"] = id;
+
             string cookie;
             if (Request.Cookies["UserId"] != null)
             {
@@ -27,52 +29,76 @@ namespace LogiranjeApp.Controllers
 
                     DatabaseService.PostLogs(obj);
 
-                    return RedirectToAction("uspehPrijava");
+                    TempData["tip"] = "Prihod";
+                    return RedirectToAction("uspeh");
                 }
                 else
                 {
-                    return RedirectToAction("odjava", new { id });
+                    HttpCookie hc = new HttpCookie("LocationId", id.ToString());
+                    Response.Cookies.Add(hc);
+                    return RedirectToAction("odhod");
                 }
             }
             else
             {
-                return RedirectToAction("prijava", new { id });
+                HttpCookie hc = new HttpCookie("LocationId", id.ToString());
+                Response.Cookies.Add(hc);
+                return RedirectToAction("prijava");
             }
         }
 
-        public ActionResult Prijava(int id)
+        public ActionResult Prijava()
         {
+            //Int32 id = (Int32)Session["locationId"];
+
             return View();
         }
 
-        public ActionResult Odjava(int id)
+        public ActionResult Odhod()
         {
-            string cookie;
+            //Int32 id = (Int32)Session["locationId"];
+
+            string cookieUserId;
+            string cookieLocationId;
             if (Request.Cookies["UserId"] == null)
             {
-                return RedirectToAction("prijava", new { id });
+                return RedirectToAction("prijava");
             }
             else
             {
-                cookie = Request.Cookies["UserId"].Value;
-                if (DatabaseService.GetLogLast(Int32.Parse(cookie), id).Count == 0 || DatabaseService.GetLogLast(Int32.Parse(cookie), id)[0].LogType != "Prijava")
+                cookieUserId = Request.Cookies["UserId"].Value;
+                cookieLocationId = Request.Cookies["LocationId"].Value;
+                if (DatabaseService.GetLogLast(Int32.Parse(cookieUserId), Int32.Parse(cookieLocationId)).Count == 0 || DatabaseService.GetLogLast(Int32.Parse(cookieUserId), Int32.Parse(cookieLocationId))[0].LogType != "Prijava")
                 {
-                    return RedirectToAction("dodaj", new { id });
+                    return RedirectToAction("dodaj", new { id = Int32.Parse(cookieLocationId) });
                 }
             }
 
+            TempData["tip"] = "Odhod";
             return View();
         }
 
-        public ActionResult UspehPrijava()
+        public ActionResult Uspeh()
         {
+            HttpCookie hc = new HttpCookie("LocationId");
+            hc.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(hc);
+
+            string tip = (string)TempData["tip"];
+            ViewBag.Tip = tip;
+
             return View();
         }
 
-        public ActionResult UspehOdjava()
-        {
-            return View();
-        }
+        //public ActionResult UspehPrijava()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult UspehOdjava()
+        //{
+        //    return View();
+        //}
 
 
     }
--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,11 +1,18 @@
 ﻿function odjava(razlog) {
 
     let odmik = document.cookie.indexOf("UserId") + 7;
-
     let userId = document.cookie.substring(odmik);
+    odmik = userId.indexOf(";");
+    if(odmik != -1) userId = userId.substring(0, odmik);
 
-    odmik = window.location.href.lastIndexOf("/") + 1;
-    let locationId = window.location.href.substring(odmik);
+
+    odmik = document.cookie.indexOf("LocationId") + 11;
+    let locationId = document.cookie.substring(odmik);
+    odmik = locationId.indexOf(";");
+    if (odmik != -1) locationId = locationId.substring(0, odmik);
+
+    console.log(userId);
+    console.log(locationId);
 
     let obj = {
         "IdUsers": userId,
@@ -20,7 +27,7 @@
         url: url,
         data: obj,
         success: function (data) {
-            window.location.href = '/logiranje/uspehOdjava'
+            window.location.href = '/logiranje/uspeh'
         },
         error: function(err) {
             alert("Prislo je do napake, poskusite ponovno!");
